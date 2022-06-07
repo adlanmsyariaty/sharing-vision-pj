e3c892fa9fb4712bf50d373e4907009ad34b762a
adlanmsyariaty <adlanmsyariaty@gmail.com>
2022-06-08 02:28:21 +0700

    succeed to create sidebar using CoreUI

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import {
+  CSidebar,
+  CSidebarBrand,
+  CSidebarNav,
+  CNavTitle,
+  CNavItem,
+  CBadge,
+  CNavGroup,
+  CSidebarToggler,
+} from "@coreui/react";
+import { useState } from "react";
 
 function App() {
+  const [visible, setVisible] = useState(false)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <CSidebar className="min-vh-100" visible={visible}>
+          <CSidebarBrand>Sidebar Brand</CSidebarBrand>
+          <CSidebarNav>
+            <CNavTitle>Nav Title</CNavTitle>
+            <CNavItem href="#">Nav item</CNavItem>
+            <CNavItem href="#">
+              With badge
+              <CBadge color="primary ms-auto">NEW</CBadge>
+            </CNavItem>
+            <CNavGroup toggler="Nav dropdown">
+              <CNavItem href="#">Nav dropdown item</CNavItem>
+              <CNavItem href="#">Nav dropdown item</CNavItem>
+            </CNavGroup>
+          </CSidebarNav>
+          <CSidebarToggler />
+        </CSidebar>
+
+      <div className="bg-primary fixed-top" onClick={() => setVisible(!visible)}>
+        <p>Change</p>
+      </div>
     </div>
   );
 }
